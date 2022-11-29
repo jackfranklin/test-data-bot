be73b03d920081a35b5d34c275ef76b7127dd392
Jack Franklin <jack@jackfranklin.net>
2022-11-29 18:54:11 +0000

    Expanding builder generation
    - perBuild
    - further oneOf
    - comments if we can't generate for the given field

--- src/generator.ts
+++ src/generator.ts
@@ -29,55 +29,97 @@ function findTargetInterface(
   return foundNode;
 }
 
+function cannotGenerateComment(field: string): string {
+  return `// ${field}: can not generate field`;
+}
+
+function generateForUnion(node: ts.UnionTypeNode): string | null {
+  const allLiterals = node.types.every((unionTypeMember) => {
+    return ts.isLiteralTypeNode(unionTypeMember);
+  });
+  if (!allLiterals) {
+    return null;
+  }
+  const possibleValues = node.types.map((unionTypeMember) => {
+    // At this point because of the allLiterals check we know this is a
+    // literal type node, but this check is here to satisfy TypeScript.
+    if (!ts.isLiteralTypeNode(unionTypeMember)) {
+      return '';
+    }
+    if (ts.isStringLiteral(unionTypeMember.literal)) {
+      return `'${unionTypeMember.literal.text}'`;
+    } else if (ts.isNumericLiteral(unionTypeMember.literal)) {
+      return unionTypeMember.literal.text;
+    } else if (unionTypeMember.literal.kind === ts.SyntaxKind.TrueKeyword) {
+      return 'true';
+    } else if (unionTypeMember.literal.kind === ts.SyntaxKind.FalseKeyword) {
+      return 'false';
+    } else {
+      // TODO: error - we don't support the kind of literal this is
+      return '';
+    }
+  });
+  return `oneOf(${possibleValues.join(', ')})`;
+}
+
 function generateFieldsForInterface(
   targetInterface: ts.InterfaceDeclaration
 ): string[] {
   const fieldCodeOutput: string[] = [];
   for (const member of targetInterface.members) {
     let fieldName = '';
-    let fieldValue = '';
+    let fieldValue: string | null = null;
     if (ts.isPropertySignature(member)) {
       if (ts.isIdentifier(member.name)) {
         fieldName = member.name.escapedText.toString();
       }
       if (member.type?.kind === ts.SyntaxKind.StringKeyword) {
         fieldValue = "''";
+      } else if (member.type?.kind === ts.SyntaxKind.BooleanKeyword) {
+        fieldValue = 'bool()';
       } else if (member.type?.kind === ts.SyntaxKind.NumberKeyword) {
         fieldValue = 'sequence()';
       } else if (member.type && ts.isUnionTypeNode(member.type)) {
-        const allLiterals = member.type.types.every((unionTypeMember) => {
-          return ts.isLiteralTypeNode(unionTypeMember);
-        });
-        // FIXME: if not all literals, show a comment explaining that we don't support that.
-        if (allLiterals) {
-          const possibleValues = member.type.types.map((unionTypeMember) => {
-            if (!ts.isLiteralTypeNode(unionTypeMember)) {
-              return '';
-            }
-            // FIXME: support number literals, etc.
-            if (ts.isStringLiteral(unionTypeMember.literal)) {
-              return `'${unionTypeMember.literal.text}'`;
-            } else if (ts.isNumericLiteral(unionTypeMember.literal)) {
-              return unionTypeMember.literal.text;
-            } else {
-              // FIXME: log that this is an unsupported type
-              return '';
-            }
-          });
-          fieldValue = `oneOf(${possibleValues.join(', ')})`;
-        }
+        fieldValue = generateForUnion(member.type);
+      } else if (member.type && ts.isArrayTypeNode(member.type)) {
+        fieldValue = `perBuild(() => {
+      return [];
+    })`;
       }
     }
-    fieldCodeOutput.push(`${fieldName}: ${fieldValue}`);
-    // ts.SyntaxKind;
+    if (fieldValue === null) {
+      fieldCodeOutput.push(cannotGenerateComment(fieldName));
+    } else {
+      fieldCodeOutput.push(`${fieldName}: ${fieldValue},`);
+    }
   }
   return fieldCodeOutput;
 }
 
+// type Indent = '\t' | '  ';
+// function indent(line, amount, spaces: boolean) {
+
+// }
+
+function indent(str: string, indentChar: IndentType, amount: number): string {
+  return indentChar.repeat(amount) + str;
+}
+
+interface GeneratorConfig {
+  tabs: boolean;
+}
+
+const enum IndentType {
+  TAB = '\t',
+  SPACE = '  ',
+}
 export function generate(
   node: ts.Node,
-  targetInterfaceName: string
+  targetInterfaceName: string,
+  config: GeneratorConfig
 ): GeneratorResult {
+  const indentChar = config.tabs ? IndentType.TAB : IndentType.SPACE;
+
   const targetInterface = findTargetInterface(node, targetInterfaceName);
   if (!targetInterface) {
     // FIXME: should probably error here.
@@ -88,11 +130,11 @@ export function generate(
   codeParts.push(
     `const ${targetInterfaceName.toLowerCase()}Builder = build<${targetInterfaceName}>({`
   );
-  codeParts.push(`  fields: {`);
+  codeParts.push(indent(`fields: {`, indentChar, 1));
   for (const fieldCode of fields) {
-    codeParts.push(`    ${fieldCode},`);
+    codeParts.push(indent(fieldCode, indentChar, 2));
   }
-  codeParts.push(`  },`);
+  codeParts.push(indent('},', indentChar, 1));
   codeParts.push(`});`);
   return { code: codeParts.join('\n') };
 }
--- test/generator.ts
+++ test/generator.ts
@@ -23,7 +23,7 @@ tap.test('generates empty strings for string types', (t) => {
      }
     })
   */
-  const result = generate(file, 'User');
+  const result = generate(file, 'User', { tabs: false });
   t.equal(
     `const userBuilder = build<User>({
   fields: {
@@ -43,7 +43,7 @@ tap.test('generates sequence() fields for number types)', (t) => {
     }
 `);
 
-  const result = generate(file, 'User');
+  const result = generate(file, 'User', { tabs: false });
   t.equal(
     `const userBuilder = build<User>({
   fields: {
@@ -56,19 +56,121 @@ tap.test('generates sequence() fields for number types)', (t) => {
   t.end();
 });
 
-tap.test('generates oneOf() for union types', (t) => {
+tap.test('generates oneOf() for union types with primitive types', (t) => {
   const file = createTypeScriptSourceFile(`
     interface User {
-      colour: 'RED'|'BLUE'|5;
+      colour: 'RED'|'BLUE'|5|true;
     }
 `);
 
-  const result = generate(file, 'User');
+  const result = generate(file, 'User', { tabs: false });
   t.equal(
     `const userBuilder = build<User>({
   fields: {
-    colour: oneOf('RED', 'BLUE', 5),
+    colour: oneOf('RED', 'BLUE', 5, true),
   },
+});`,
+    result.code
+  );
+  t.end();
+});
+
+tap.test('generates bool() for boolean types', (t) => {
+  const file = createTypeScriptSourceFile(`
+    interface User {
+      isAdmin: boolean;
+    }
+`);
+
+  const result = generate(file, 'User', { tabs: false });
+  t.equal(
+    `const userBuilder = build<User>({
+  fields: {
+    isAdmin: bool(),
+  },
+});`,
+    result.code
+  );
+  t.end();
+});
+
+tap.test('refuses to generate oneOf calls for complex union types', (t) => {
+  const file = createTypeScriptSourceFile(`
+    interface User {
+      isAdmin: 'foo'|{};
+    }
+`);
+
+  const result = generate(file, 'User', { tabs: false });
+  t.equal(
+    `const userBuilder = build<User>({
+  fields: {
+    // isAdmin: can not generate field
+  },
+});`,
+    result.code
+  );
+  t.end();
+});
+
+tap.test(
+  "generates a comment if it doesn't know how to generate the field value",
+  (t) => {
+    const file = createTypeScriptSourceFile(`
+    interface User {
+      isAdmin: {};
+    }
+`);
+
+    const result = generate(file, 'User', { tabs: false });
+    t.equal(
+      `const userBuilder = build<User>({
+  fields: {
+    // isAdmin: can not generate field
+  },
+});`,
+      result.code
+    );
+    t.end();
+  }
+);
+
+tap.test('wraps object-like fields in a perBuild() call', (t) => {
+  const file = createTypeScriptSourceFile(`
+    interface User {
+      isAdmin: string[]
+    }
+`);
+
+  const result = generate(file, 'User', { tabs: false });
+  t.equal(
+    `const userBuilder = build<User>({
+  fields: {
+    isAdmin: perBuild(() => {
+      return [];
+    }),
+  },
+});`,
+    result.code
+  );
+  t.end();
+});
+
+tap.skip('it can output code using tabs', (t) => {
+  const file = createTypeScriptSourceFile(`
+    interface User {
+      isAdmin: string[]
+    }
+`);
+
+  const result = generate(file, 'User', { tabs: true });
+  t.equal(
+    `const userBuilder = build<User>({
+\tfields: {
+\t\tisAdmin: perBuild(() => {
+\t\t\treturn [];
+\t\t}),
+\t},
 });`,
     result.code
   );
